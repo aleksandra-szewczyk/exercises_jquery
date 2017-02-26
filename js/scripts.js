79360137ca18521fe55d8d05734eb0c597b37c9f
Łukasz Sałajczyk <lukasz.salajczyk@jazzy.pro>
2017-02-26 11:12:41 +0100

    Fix prev button

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,38 @@
-$(function() {
-   var carouselList = $('#carousel ul');
-    
-    function changeSlideNext() {
-        carouselList.animate({'marginLeft':-900}, 500, moveFirstSlideAfterLast);
-    };
-  
+$(function () {
+    var carouselList = $('#carousel ul');
+
+    function next() {
+        carouselList.animate({'marginLeft': -900}, 500, moveFirstSlideAfterLast);
+    }
+
     function moveFirstSlideAfterLast() {
-        
-        (carouselList.find('li:last')).after(carouselList.find('li:first'));
-        carouselList.css({'marginLeft':0});
-    };
-    
-    function changeSlidePrevious() {
-        carouselList.animate({'marginLeft':0}, 500);
-    };
-    
-    function moveLastSlideBeforeFirst() {
-        (carouselList.find('li:first')).before(carouselList.find('li:last'));
-        carouselList.css({'marginLeft': -900}, changeSlidePrevious)
-    };
-    
-    setInterval(changeSlideNext, 4000);
-    $('#next').click(function() {
-        changeSlideNext();
-    });
-    $('#previous').click(function() {
+        var lastItem    = carouselList.find('li:last');
+        var firstItem   = carouselList.find('li:first');
+
+        lastItem.after(firstItem);
+        carouselList.css({'marginLeft': 0});
+    }
+
+    function prev() {
         moveLastSlideBeforeFirst();
+        carouselList.css('marginLeft', -900);
+
+        carouselList.animate({'marginLeft': 0}, 500);
+    }
+
+    function moveLastSlideBeforeFirst() {
+        var lastItem    = carouselList.find('li:last');
+        var firstItem   = carouselList.find('li:first');
+
+        firstItem.before(lastItem);
+    }
+
+    setInterval(next, 4000);
+
+    $('#next').click(function () {
+        next();
     });
-        
+    $('#previous').click(function () {
+        prev();
+    });
 });
